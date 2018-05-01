767531ef3a4b090d1bc0963e687b5215d6f92f53
Munis Isazade <munisisazade@gmail.com>
2018-05-01 19:46:04 +0400

    Ready project

--- celery.dockerfile
+++ celery.dockerfile
@@ -6,17 +6,17 @@ ENV PYTHONUNBUFFERED 1
 ENV APP_USER myapp
 ENV APP_ROOT /src
 RUN mkdir /src;
-RUN groupadd -r  \
+RUN groupadd -r ${APP_USER} \
     && useradd -r -m \
-    --home-dir  \
+    --home-dir ${APP_ROOT} \
     -s /usr/sbin/nologin \
-    -g  
+    -g ${APP_USER} ${APP_USER}
 
-WORKDIR 
+WORKDIR ${APP_ROOT}
 
 RUN mkdir /config
 ADD requirements.txt /config/
 RUN pip install --no-cache-dir -r /config/requirements.txt
 
-USER 
-ADD . 
+USER ${APP_USER}
+ADD . ${APP_ROOT}
--- diplom/settings.py
+++ diplom/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'z74ibn9+b-%$g=4=)z5bt@@m#{SECRET_KEY}k#!u9!)dl^h!(7y1auk_#{SECRET_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PROD = not DEBUG
+PROD = DEBUG
 
 ALLOWED_HOSTS = ['*'] if PROD else ['*']
 
@@ -50,11 +50,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-    # 'social_core',  # Python Social auth
-    # 'social_django',  # Django Social auth
-    # 'celery',  # Don't forget to add celery
-    # 'django_celery_results',  # celery result
-    # 'django_celery_beat',
+    'social_core',  # Python Social auth
+    'social_django',  # Django Social auth
+    'celery',  # Don't forget to add celery
+    'django_celery_results',  # celery result
     'ckeditor',
     'ckeditor_uploader',
     'widget_tweaks',
@@ -168,35 +167,35 @@ WSGI_APPLICATION = 'diplom.wsgi.application'
 
 # Authentication backend
 # If you have social login activate this
-# AUTHENTICATION_BACKENDS = (
-#     'social_core.backends.github.GithubOAuth2',
-#     'social_core.backends.twitter.TwitterOAuth',
-#     'social_core.backends.facebook.FacebookOAuth2',
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.facebook.FacebookOAuth2',
 
-#     'django.contrib.auth.backends.ModelBackend',
-# )
+    'django.contrib.auth.backends.ModelBackend',
+)
 
-# SOCIAL_AUTH_PIPELINE = (
-#     'social_core.pipeline.social_auth.social_details',
-#     'social_core.pipeline.social_auth.social_uid',
-#     'social_core.pipeline.social_auth.social_user',
-#     'social_core.pipeline.user.get_username',
-#     'social_core.pipeline.user.create_user',
-#     'social_core.pipeline.social_auth.associate_user',
-#     'social_core.pipeline.social_auth.load_extra_data',
-#     'social_core.pipeline.user.user_details',
-#     'social_core.pipeline.social_auth.associate_by_email',
-#     'app.pipeline.get_avatar', # custom pipeline
-# )
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
+    # 'app.pipeline.get_avatar', # custom pipeline
+)
 
-# SOCIAL_AUTH_LOGIN_ERROR_URL = '/'
-# SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/dashboard/'
-# SOCIAL_AUTH_FACEBOOK_KEY = '******************'  # App ID
-# SOCIAL_AUTH_FACEBOOK_SECRET = '****************************' # App Secret
-# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_friends']
-# SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
-#     'fields': 'id,name,email',
-# }
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/dashboard/'
+SOCIAL_AUTH_FACEBOOK_KEY = '******************'  # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '****************************' # App Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id,name,email',
+}
 
 # logout redirect url
 LOGOUT_URL = 'logout'
